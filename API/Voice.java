fa56c1fc0c091698b03e5b3f2edac6961cf8691e
Teagan Marie Glenn <tophurious@gmail.com>
2012-12-09 12:54:33 -0700

    Added API calls
    Voice:
    Changed private variables to avoid scope issues (I like _varName but if
    you guys don't feel free to change it)
    Added specific constructors
    Login return type should be boolean (we need to know if it worked :-) )
    Changed "getAllSMS" to "getText" to match Voice labels
    Added missing Voice labels
    Added item actions - Delete, Restore, Star/Unstar, Spam/Not Spam,
    Archive/Un-archive (though I think Voice has done away with Archived
    messages)
    
    SMS (Message):
    Changed class "SMS" to "Message" to accomodate different message types
    (SMS, Recorded Call and Voicemail)
    Added attributes
    
    AudioMessage:
    Created - I have never dealt with MP3/Ogg with Java so left as TODO
    
    Person:
    Changed private variables to prevent scope issues.

--- API/Voice.java
+++ API/Voice.java
@@ -3,27 +3,65 @@ import android.net.http.AndroidHttpClient;
 
 public class Voice
 {
-    private String username = null;
-    private String password = null;
-    private String phoneNumber = null;
-    
+    private String _username = null;
+    private String _password = null;
+    private String _phoneNumber = null;
+	    
     public Voice()
     {
     }
+	
+	public Voice(String username, String password)
+	{
+	}
+	
+	public Voice(String username, String password, String phonenumber)
+	{
+	
+	}
     
-    public void login()
+	/**
+	* 
+	*
+	* @return	Whether the login request was successful
+	*/
+    public boolean login()
     {
     }
+	
     
     public List<Conversation> getInbox()
     {
     }
+	
     public List<Conversation> getStarred()
     {
     }
-    public List<Conversation> getAllSMS()
+	
+    public List<Conversation> getText()
     {
     }
+	
+	public List<Conversation> getHistory()
+	{
+	}
+	
+	public List<Conversation> getRecordedCalls()
+	{
+	}
+	
+	public List<Conversation> getPlacedCalls()
+	{
+	}
+	
+	public List<Conversation> getReceivedCalls()
+	{
+	}
+	
+	public List<Conversation> getMissedCalls()
+	{
+	}
+	
     public List<Conversation> getTrash()
     {
     }
@@ -40,28 +78,95 @@ public class Voice
         // remove conversations not from person
     }
     
-    public boolean sendSMS(Person to, String message)
+    public boolean sendSMS(Message msg)
     {
     }
+	
+	/**
+	*
+	* @param	msg	Item to be deleted
+	* @return	Whether the item was deleted
+	*/
+	public boolean deleteItem(Message msg)
+	{
+	}
+	
+	/**
+	* Restores an item from trash
+	*
+	* @param	msg	Item to be restored
+	* @return	Whether the item was restored
+	*/
+	public boolean retoreItem(Message msg)
+	{	
+	}
+	
+	/**
+	* Stars or un-stars an item
+	*
+	* @param	msg	Item to be starred/un-starred
+	* @return	Whether the item was successfully starred/un-starred
+	*/
+	public boolean toggleStar(Message msg)
+	{
+	}
+	
+	/**
+	* Marks or un-marks as spam
+	*
+	* @param	msg	Item to be marked spam/not spam
+	* @return	Whether the item was successfully marked/unmarked as spam
+	*/
+	public boolean toggleSpam(Message msg)
+	{
+	}
+	
+	public boolean toggleRead(Message msg)
+	{
+	
+	}
+	
+	private boolean archive(Message msg)
+	{
+	}
+	
+	private boolean unarchive(Message msg)
+	{
+	}
+	
+	private JSONObject execute(String command)
+	{
+	}
 }
 
 /*
- *  A conversation is a grouping of single back-and-forth SMS messages.
+ *  A conversation is a grouping of single back-and-forth messages.
  *  Google Voice groups them for us.
  */
 public class Conversation
 {
-    private List<SMS> messages;
+	private Person _contact;
+    private List<Message> _messages;
 }
 
 /*
- *  A single SMS message.
+ *  A single message.
  */
-public class SMS
+public class Message
 {
-    private Person from = null;
-    private Date date = null;
-    private String message = null;
+    private Person _from = null;
+    private Date _date = null;
+	private String _transcript = null;
+	private String _id = null;
+	private boolean _isStarred = false;
+	private boolean _isSpam = false;
+	private boolean _isRead = false;
+	private boolean _isNew = false;
+}
+
+public class AudioMessage extends Message
+{
+	// TODO: store mp3 and ogg for recorded calls and voicemails
 }
 
 /*
@@ -71,7 +176,7 @@ public class SMS
  */
 public class Person
 {
-    private String name = null;
-    private String nickname = null;
-    private String phoneNumber = null;
+    private String _name = null;
+    private String _nickname = null;
+    private String _phoneNumber = null;
 }
