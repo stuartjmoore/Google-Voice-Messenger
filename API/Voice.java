af9e7b11cbbb9951633c41e340ba02633b0905b5
Stuart Moore <stuart@informationarchitects.jp>
2012-12-12 09:35:56 +0100

    Added name strings.

--- API/Voice.java
+++ API/Voice.java
@@ -190,7 +190,9 @@ public class Person
 {
     private String _lookupKey = null; // Key to connect GV contact to device list
     
-    private String _nameOnDevice = null;
+    private String _nameDisplay = null;
+    private String _nameGiven = null; // From Contacts database, may be needed for
+    private String _nameFamily = null; // UI bolding and wording
     private String _nameOnServer = null;
     
     private String _phoneNumber = null;
