7c1b3b08e4204910cbf44589c83d6aa60c3097d3
Teagan Marie Glenn <tophurious@gmail.com>
2012-12-14 12:36:50 -0700

    New unit testing cases

---
+++ unittest/temp/gvm/api/VoiceTest.java
@@ -0,0 +1,176 @@
+/**
+ * 
+ */
+package temp.gvm.api;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+/**
+ * @author tglenn
+ *
+ */
+public class VoiceTest {
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@BeforeClass
+	public static void setUpBeforeClass() throws Exception {
+	}
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@AfterClass
+	public static void tearDownAfterClass() throws Exception {
+	}
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	/**
+	 * Test method for {@link temp.gvm.api.Voice#Voice()}.
+	 */
+	@Test
+	public void testVoice() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	/**
+	 * Test method for {@link temp.gvm.api.Voice#Voice(java.lang.String, java.lang.String)}.
+	 */
+	@Test
+	public void testVoiceStringString() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	/**
+	 * Test method for {@link temp.gvm.api.Voice#Voice(java.lang.String, java.lang.String, java.lang.String)}.
+	 */
+	@Test
+	public void testVoiceStringStringString() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	/**
+	 * Test method for {@link temp.gvm.api.Voice#login()}.
+	 */
+	@Test
+	public void testLogin() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	/**
+	 * Test method for {@link temp.gvm.api.Voice#getInbox()}.
+	 */
+	@Test
+	public void testGetInbox() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	/**
+	 * Test method for {@link temp.gvm.api.Voice#getStarred()}.
+	 */
+	@Test
+	public void testGetStarred() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	/**
+	 * Test method for {@link temp.gvm.api.Voice#getText()}.
+	 */
+	@Test
+	public void testGetText() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	/**
+	 * Test method for {@link temp.gvm.api.Voice#getTrash()}.
+	 */
+	@Test
+	public void testGetTrash() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	/**
+	 * Test method for {@link temp.gvm.api.Voice#getSpam()}.
+	 */
+	@Test
+	public void testGetSpam() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	/**
+	 * Test method for {@link temp.gvm.api.Voice#getHistory()}.
+	 */
+	@Test
+	public void testGetHistory() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	/**
+	 * Test method for {@link temp.gvm.api.Voice#getRecordedCalls()}.
+	 */
+	@Test
+	public void testGetRecordedCalls() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	/**
+	 * Test method for {@link temp.gvm.api.Voice#getPlacedCalls()}.
+	 */
+	@Test
+	public void testGetPlacedCalls() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	/**
+	 * Test method for {@link temp.gvm.api.Voice#getReceivedCalls()}.
+	 */
+	@Test
+	public void testGetReceivedCalls() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	/**
+	 * Test method for {@link temp.gvm.api.Voice#getMissedCalls()}.
+	 */
+	@Test
+	public void testGetMissedCalls() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	/**
+	 * Test method for {@link temp.gvm.api.Voice#search(java.lang.String)}.
+	 */
+	@Test
+	public void testSearch() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	/**
+	 * Test method for {@link temp.gvm.api.Voice#getConversationsFrom(temp.gvm.api.Person)}.
+	 */
+	@Test
+	public void testGetConversationsFrom() {
+		fail("Not yet implemented"); // TODO
+	}
+
+}
